8328aff0b16728c369d619294658173896e35272
Joonsup Lee <flamewizkey@naver.com>
2026-07-18 05:30:06 +0900

    feat : 설정파일 추가

---
+++ TripFlow-backend/src/main/java/com/tripflow/TripFlowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tripflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripFlowBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripFlowBackendApplication.class, args);
+    }
+
+}
---
+++ TripFlow-backend/src/test/java/com/tripflow/TripFlowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripFlowBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
